9a8015a25f8affc2127f7b734593eaa4ab599636
Subeg <himalsubeg@gmail.com>
2024-02-13 16:57:12 +0545

    work on array object and json js from office

--- basics/data-types.js
+++ basics/data-types.js
@@ -51,3 +51,39 @@ console.log(typeof l);
 
 let m;
 console.log(typeof m)
+
+// array
+// comma seperated
+// index => value pair
+// index default defined
+// index always start from 0
+// 0-9
+// size = no of elements
+// last index = size -1
+
+let employee_1 = ["Employee Name","Kathmandu", 1231231231, "Client"];
+console.log(employee_1[0])
+//size = employee_1 =>4
+// last index = 4-1 =3
+let employee_2 = new Array("Employee Name2","Lalitpur",12312312,"Admin");
+console.log(employee_2[1])
+
+// product ===> name, brand, catagory, price, discount, after_discount
+// values print
+
+// object data (prototype based object oriented)
+
+let employee_obj_1 = {
+    name: "Employee Name",
+    address: "Kathmandu",
+    phone : 1212312312,
+    position : "client"
+};
+console.log(employee_obj_1.name)
+let employee_obj_2 = {
+    name: "Employee Name",
+    address: "Kathmandu",
+    phone : 1212312312,
+    position : "client"
+};
+console.log(employee_obj_2["address"])
