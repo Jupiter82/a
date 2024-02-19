a2fe7c1282539c6f2dcedc45a13762862ed6025c
Subeg <himalsubeg@gmail.com>
2024-02-19 16:45:49 +0545

    array all functions

---
+++ basics/array-t2.js
@@ -0,0 +1,24 @@
+// create an array variable to store students information
+// A single student should contain, name, markobtained, email, address keys
+// Every students is an object.
+// Insert at least 10 students profile to array varibale created above using, push, unshift, or
+// concat options
+// try to read from top, and from end also from middle of the array
+
+
+//declare variable
+let students = []
+
+let student_1 = {
+    name: "Student One",
+    mark: 500,
+    email: "one@gmail.com",
+    address: "Thimi"
+}
+
+let student_2 = {
+    name: "Student Two",
+    mark: 400,
+    email: "two@gmail.com",
+    address:"Lagankhel"
+}
--- basics/array.js
+++ basics/array.js
@@ -76,60 +76,74 @@ users[5] = user_6 //populate index 5 if non-existing
 console.log(users);
 console.log(users.length)//length properties;
 
+users[4] = user_4
+console.log(users);
+
+//declare variable user_7
+let user_7 = {
+  name: "User Seven",
+  email: "seven@user.com",
+  address: "Bhaktapur",
+  position:  "user"
+}
 
 
-// let products = [];
-// let prod1 = {
-//   name: "Product 1",
-//   price: 1000,
-//   discount: 10,
-// };
-// let prod2 = {
-//   name: "Product 2",
-//   price: 2000,
-//   discount: 10,
-// };
-// let prod3 = {
-//   name: "Product 3",
-//   price: 3000,
-//   discount: 10,
-// };
-// let prod4 = {
-//   name: "Product 4",
-//   price: 4000,
-//   discount: 10,
-// };
-// let prod5 = {
-//   name: "Product 5",
-//   price: 5000,
-//   discount: 10,
-// };
-// //
-// products.push(prod1, prod2, prod3, prod4, prod5);
+//inser object in middle of array
+//declare variable indx_2
+let indx_2 = users.splice(0,3) //gives top index 0 1 2 
+indx_2.push(user_7)  //0 1 2 user_7 at end of index
+// users.unshift(user_7) // user_7 3 4 5 6 at the start of index
 
-// var discountAmt = (products[0].price * products[0].discount) / 100;
-// var afterDiscount = products[0].price - discountAmt;
-// products[0].afterDis = afterDiscount;
-// products[0].disAmt = discountAmt;
+//repopulate users
+users = indx_2.concat(users)
+console.log(users)
 
-// var discountAmt = (products[1].price * products[1].discount) / 100;
-// var afterDiscount = products[1].price - discountAmt;
-// products[1].afterDis = afterDiscount;
-// products[1].disAmt = discountAmt;
 
-// var discountAmt = (products[2].price * products[2].discount) / 100;
-// var afterDiscount = products[2].price - discountAmt;
-// products[2].afterDis = afterDiscount;
-// products[2].disAmt = discountAmt;
 
-// var discountAmt = (products[3].price * products[3].discount) / 100;
-// var afterDiscount = products[3].price - discountAmt;
-// products[3].afterDis = afterDiscount;
-// products[3].disAmt = discountAmt;
+//fetch
+// variable declaration first
+// let first = users.shift();   // 0 index return
+// console.log(first)
 
-// var discountAmt = (products[4].price * products[4].discount) / 100;
-// var afterDiscount = products[4].price - discountAmt;
-// products[4].afterDis = afterDiscount;
-// products[4].disAmt = discountAmt;
+//variable declaration last
+// let last = users.pop() // last index value
+// console.log(last)
 
-// console.log(products);
+// let index_1 = users.splice(1,2)
+// console.log(index_1)
+// console.log(users)
+
+//declare variable
+// let index_1 = [users[1],users[2]]
+// console.log(index_1)
+
+// let index_1 = users.slice(1,3)
+// console.log(index_1);
+
+// let index_1 = users.slice(1,2)
+// let index_2 = users.slice(2,3)
+
+// console.log(index_1,index_2);
+
+// index_1.push(index_2) //error 
+// let arr = index_1.concat(index_2)
+// console.log(arr);
+
+
+//file upload
+//declare varaible allowed
+let allowed = ['jpg','png','jpeg','webp'] //array single dimentinal
+let ext = "png";
+
+allowed.includes(ext) //boolean
+console.log(allowed.includes(ext));
+
+allowed.indexOf(ext) //-1, or index
+console.log(allowed.indexOf(ext))
+
+allowed.sort()
+console.log(allowed.sort());
+
+console.log(allowed.reverse());
+
+// .map()
