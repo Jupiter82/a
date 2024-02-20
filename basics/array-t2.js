f78734cff2d0f5dcfb571531717f647208036a16
Subeg <himalsubeg@gmail.com>
2024-02-20 17:38:38 +0545

    task array t2 from office

--- basics/array-t2.js
+++ basics/array-t2.js
@@ -15,10 +15,65 @@ let student_1 = {
     email: "one@gmail.com",
     address: "Thimi"
 }
-
 let student_2 = {
     name: "Student Two",
     mark: 400,
     email: "two@gmail.com",
     address:"Lagankhel"
 }
+let student_3 = {
+    name: "Student Three",
+    mark: 400,
+    email: "two@gmail.com",
+    address:"Lagankhel"
+}
+let student_4 = {
+    name: "Student Four",
+    mark: 400,
+    email: "two@gmail.com",
+    address:"Lagankhel"
+}
+let student_5 = {
+    name: "Student Five",
+    mark: 400,
+    email: "two@gmail.com",
+    address:"Lagankhel"
+}
+let student_6 = {
+    name: "Student Six",
+    mark: 400,
+    email: "two@gmail.com",
+    address:"Lagankhel"
+}
+let student_7 = {
+    name: "Student Seven",
+    mark: 400,
+    email: "two@gmail.com",
+    address:"Lagankhel"
+}
+let student_8 = {
+    name: "Student Eight",
+    mark: 400,
+    email: "two@gmail.com",
+    address:"Lagankhel"
+}
+let student_9 = {
+    name: "Student Nine",
+    mark: 400,
+    email: "two@gmail.com",
+    address:"Lagankhel"
+}
+let student_10 = {
+    name: "Student Ten",
+    mark: 400,
+    email: "two@gmail.com",
+    address:"Lagankhel"
+}
+
+
+students.push(student_1,student_2)
+console.log(students)
+students.unshift(student_4,student_3)
+console.log(students)
+students.concat(student_6,student_7)
+console.log(students)
