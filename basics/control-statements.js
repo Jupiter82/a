9214034d699e578ed710063f18df5124c64c0002
Subeg <himalsubeg@gmail.com>
2024-02-22 17:06:47 +0545

    worked oon operators completly

--- basics/control-statements.js
+++ basics/control-statements.js
@@ -1,13 +1,6 @@
-let day ="Sunday"
-
-if(day === "Sunday" || day === 'Saturday' ){
-    console.log("Holiday")
-}
-else{
-    if(day === 'Friday'){
-        console.log("Weekend")
-    }
-    else{
-        console.log("Weekday")
-    }
-}
+/*
+ if-else
+ else-if
+ switch-case
+ loop
+*/
--- basics/operators.js
+++ basics/operators.js
@@ -9,6 +9,13 @@
     < lessthan, > greaterthan, <= lessthan equal to, >= greater than equal to, ==, != not equal to, ===, !==
     e. String / Concatination Operation
         +, comma(,)
+    f. Logical Operator(boolean)
+        && (and), ||(or), !(not)
+    g. Ternary/conditional Operator
+      (expression) ? true condition (in 1 line / single statement) : false Expression
+    h. Spread or Rest Operator
+    ...
+
 */
 
 //declare variable a , b , c , e,f,g
@@ -99,3 +106,99 @@ console.log(name);
 //email service
 let message =  'Hellow there,'
 message += 'You have a message' //message = message + 'You have a message' (concatination operation)
+
+//reassigns value of x=10 and y =15
+x = 10
+y = 15;
+//< lessthan // && both expression should be true // || only one expression need to be true 
+((x === y) && (x < y) && (x > y))
+//x === y false && x < y true && x > y false => false
+
+
+
+console.log(((x === y) || (x < y) || (x > y)),'test');
+// false || true || false => true
+
+
+// !(false || true || false) => !(true) => false
+console.log(!((x === y) || (x < y) || (x > y)),"test")
+
+//Ternary / Conditional Operator
+
+// declare variable student_1
+let student_1 = { //object
+    //key or property : value
+    name:"Std One",
+    marksObt: 120,
+    // email: "test@test.com"
+}
+
+// email ===> email , null
+
+//declare email variable
+// let email = (student_1.hasOwnProperty('email') === true) ? student_1.email : null; // object.email(acess object email key or property)
+// let email = (student_1.hasOwnProperty('email')) ? student_1.email : null
+// let email = student_1.email ? student_1.email : null;
+//Assign
+// let email = student_1.email ?? null
+let email = student_1.email || null
+
+
+console.log(email); //undefined
+
+//declare variable user_1
+let user_1 ={ //object
+    //key or property : value
+    name:"User One",
+    email:"one@user.com",
+    address:"Kathmandu"
+}
+
+let admin_1 = {
+    // name: user_1.name,
+    // email: user_1.email,
+    // address:user_1.address,
+    ... user_1, //spread Operation
+    role:"admin",
+    organization: "Broadway Infosys "
+}
+
+console.log(admin_1)
+
+//object destructuring
+let adminaccess = {name: fullname/*aliasing destructured variables = renaming or creating alternative names for variables */, email: emailAdress, role, ...other/*...rest operators*/} = admin_1
+console.log(fullname)
+console.log(emailAdress)
+console.log(role)
+console.log(other)
+// let fullname = admin_1.name
+// let emailAdress = admin_1.email
+// let role = admin_1.role
+
+// let other ={
+//     organization:admin_1.organization,
+//     address:admin_1.address
+// }
+
+
+
+
+
+
+
+
+
+let user_2 = {...user_1}//deep cloning spread operator
+
+user_2.name = "User Two";
+console.log(user_2); // name :"User two" after deep cloning using spread operators ==>  name :"User Two"
+console.log(user_1); // name :"User two" after deep cloning using spread operators ==>  name :"User One"
+
+
+//declare variable arr_1 and arr_3
+let arr_1 = [1,2,3]
+let arr_2 = [4,5,6]
+
+let arr_3 = [...arr_1, ...arr_2] // [1,2,3,4,5,6]
+console.log(arr_3)
+
