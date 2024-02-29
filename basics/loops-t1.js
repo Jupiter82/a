5e0227783c8387b3114f8bb80ec2966cc742cf69
Subeg <himalsubeg@gmail.com>
2024-02-29 15:31:23 +0545

    worked on do while loop

--- basics/loops-t1.js
+++ basics/loops-t1.js
@@ -1,24 +1,56 @@
-let i = 1;
-//loop
-do{//loop body
-    console.log(i)
-    i++
-}while(i <= 10)
+// Product
+// Create an array in js to store Product detail
+// A Products should contain name,price,discount in %
+// Calculate discount amount and after discount and assign to the product itself
+// Print the product detail in the following Format:
+// Name : ........
+// Price: Npr .....
+// Discount: ...... %
+// Discount Amt : Npr .......
+// After DIscount : Npr ....
+
+// Viber sir number(Sandesh bhattarai): 9802111635
 
 
-//csv Import
-//read
-let students = [
-    {
-        name: "Student",
-        marksObt:234
-    },
-    {
-        name: "Student 1",
-        marksObt:345
-    },
-    {
-        name: "Student 2",
-        marksObt:420
-    },  
-]
+let products = [];
+let prod1 = {
+  name: "Product 1",
+  price: 1000,
+  discount: 10,
+};
+let prod2 = {
+  name: "Product 2",
+  price: 2000,
+  discount: 10,
+};
+let prod3 = {
+  name: "Product 3",
+  price: 3000,
+  discount: 10,
+};
+let prod4 = {
+  name: "Product 4",
+  price: 4000,
+  discount: 10,
+};
+let prod5 = {
+  name: "Product 5",
+  price: 5000,
+  discount: 10,
+};
+//
+products.push(prod1, prod2, prod3, prod4, prod5);
+
+//declare variables
+let i = 0//acces 0 index of array
+let size = products.length //length of array
+do {
+    
+} while (i < size);
+
+var discountAmt = (products[0].price * products[0].discount) / 100;
+var afterDiscount = products[0].price - discountAmt;
+products[0].afterDis = afterDiscount;
+products[0].disAmt = discountAmt;
+
+console.log(products);
--- basics/loops.js
+++ basics/loops.js
@@ -18,7 +18,8 @@ do{
     //loop body
     console.log(i)
     i++
-}while(i <= 10); 
+}while(i <= 10); //defines how many times loop runs// true loop runs// false loop stops
+// advantage of do while loop =>loop runs at least one time even when expression does not meet
 
 
 
@@ -47,4 +48,64 @@ do{
 // console.log(3)
 // console.log(4)
 // console.log(5)
+
+ 
+{/* <ul>
+//Be API Call ===> Data ===> Array of objects
+//loop js
+
+  <li>
+    <a href="">
+
+    </a>
+  </li>
+</ul> */}
+
+//CSV(comma seperated value) Import
+//read
+//declare variable students
+let students = [//array
+    {//objects
+        //key or property : value
+        name: "Student",
+        markObt: 345
+    },
+    {
+        name: "Student 1",
+        markObt:420
+    }
+]
+
+// do while
+ i = 0 // 0 because need too access 0 index //reassign i =0
+ let size = students.length; //total size of array
+do{
+//
+students[i].percent = students[i].markObt/500*100
+if (students[i].percent >= 80/*expression*/) {
+    students[i].division = "Distinction"
+}else{
+    if(students[i].percent >= 60){
+        students[i].division = "First Division"
+    }else{
+        if (students[i].percent >= 45) {
+          students[i].division = "Second Division"
+        }else{
+          if (students[i].percent >= 32) {
+              students[i].division = "Third Division"
+          }else{
+              students[i].division = "Sorry you have failed"
+          }
+        }
+    }
+}
+//concatination
+console.log("Name:" + students[i].name );
+console.log( "Mark:" , students[i].markObt );
+console.log("Percentage:" + students[i].percent );
+console.log("Division:" + students[i].division );
+console.log(" ");
+
+i++
+}while(i < size)  //if <=less than equal too 0,1,2 run loop //< less than 0,1 run loop
 
