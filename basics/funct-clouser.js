67f2597f5ada7baae29d085b65a542a0b22a3ccd
Subeg <himalsubeg@gmail.com>
2024-03-18 10:09:05 +0545

    push from office 3-18

--- basics/funct-clouser.js
+++ basics/funct-clouser.js
@@ -1,15 +1,21 @@
-//clouser
+//clouser = define within another function
 // Async
 
 const func1 = () => {
     //delay 3 sec
-    console.log("I am on function 1");
+    console.log("I am on function 1")
+    let result = ""
+
+    const func2 = () => {
+        //2sec
+        console.log("I am  on function 2");
+    }
+    func2(result) // async ===> BP 2
+
+
 }
 
-const func2 = () => {
-    //2sec
-    console.log("I am  on function 2");
-}
+
 
 const func3 = () => {
     //1sec 
@@ -18,7 +24,6 @@ const func3 = () => {
 
 //***------------------------------------------------------***/
 
-func1() //callfunction //if async ===> BP 1
-func2() // async ===> BP 2
+let res = func1() //callfunction //if async ===> BP 1
 func3() // not async ===> Outp
 //Output
---
+++ basics/funct-clousers-t1.js
@@ -0,0 +1,2 @@
+// Create a function called calculate volume which accepts 3 args leng,width,
+//create another function withing the same to calcuate area
